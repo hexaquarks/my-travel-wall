3269b2f35a41227af335eaee7f758f80ff87ade7
hexaquarks <seseriusca@gmail.com>
2024-10-09 17:50:17 -0400

    Implement add and remove functionality for cards

--- client/src/lib/components/CountryCard.svelte
+++ client/src/lib/components/CountryCard.svelte
@@ -1,13 +1,29 @@
-<script>
+<script lang="ts">
     import { Card, Button } from "flowbite-svelte";
-    import { PlusOutline } from "flowbite-svelte-icons";
+    import { PlusOutline, MinusOutline } from "flowbite-svelte-icons";
+
+    export let key: string = "";
+    export let isPlaceholder: boolean;
+    export let onAddCard: () => void;
+    export let onDeleteCard: (id: string) => void;
 </script>
 
-<Card size="md" class="items-center">
-    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-        Add a country
-    </p>
-    <Button class="w-fit">
-        <PlusOutline class="w-6 h-6 ms-2 text-white" />
-    </Button>
-</Card>
+{#if isPlaceholder}
+    <Card size="md" class="items-center">
+        <p
+            class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight"
+        >
+            Add a country
+        </p>
+        <Button class="w-fit" on:click={onAddCard}>
+            <PlusOutline class="w-6 h-6 ms-2 text-white" />
+        </Button>
+    </Card>
+{:else}
+    <Card size="md" class="items-center">
+        <p>{key}</p>
+        <Button class="w-fit" on:click={() => onDeleteCard(key)}>
+            <MinusOutline class="w-6 h-6 ms-2 text-white" />
+        </Button>
+    </Card>
+{/if}
--- client/src/routes/wall/+page.svelte
+++ client/src/routes/wall/+page.svelte
@@ -1,8 +1,37 @@
-<script>
+<script lang="ts">
+    import { v4 as uuid } from "uuid";
     import CountryCard from "$lib/components/CountryCard.svelte";
+
+    type CountryCardType = {
+        id: string;
+    };
+
+    let countryCards: CountryCardType[] = [];
+
+    const addCard = () => {
+        const newCard: CountryCardType = { id: uuid() };
+        countryCards = [...countryCards, newCard];
+    };
+
+    const deleteCard = (id: string) => {
+        countryCards = countryCards.filter(
+            (countryCard) => countryCard.id != id,
+        );
+    };
 </script>
 
 <div class="w-screen h-full mt-5 flex flex-col gap-5 items-center">
-    <CountryCard />
-    <CountryCard />
+    {#each countryCards as card (card.id)}
+        <CountryCard
+            key={card.id}
+            isPlaceholder={false}
+            onAddCard={addCard}
+            onDeleteCard={deleteCard}
+        />
+    {/each}
+    <CountryCard
+        isPlaceholder={true}
+        onAddCard={addCard}
+        onDeleteCard={deleteCard}
+    />
 </div>
