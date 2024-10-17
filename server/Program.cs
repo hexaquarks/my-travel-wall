40ffcab6047fa1247da8dcf6bcd39959490c7d35
hexaquarks <seseriusca@gmail.com>
2024-10-17 17:54:40 -0400

    Add a prototype of an MVC in C# backend to communicate with a local mongodb instance

--- server/Program.cs
+++ server/Program.cs
@@ -1,15 +1,25 @@
+using UserDataApi.Models;
+using UserDataApi.Services;
+/* using UserDataApi.Controllers; */
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.Configure<UserDataDatabaseSettings>(
+    builder.Configuration.GetSection("UserDataDatabase"));
+builder.Services.AddSingleton<UsersService>();
 
 var app = builder.Build();
 
-// Only map API endpoints
+// Only map API endpoint
 app.UseRouting();
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+/* app.UseAuthorization(); */
+app.MapControllers();
+/* app.UseEndpoints(endpoints => */
+/* { */
+/*     endpoints.MapControllers(); */
+/* }); */
 
 app.Run();
