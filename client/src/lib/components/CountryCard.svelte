5a6c334fd700bb2175d29bd4b9d3050bf33aecd2
Mihail Anghelici <seseriusca@gmail.com>
2024-11-03 13:51:10 -0500

    Add a picture navigation prototype in the country cards (#12)
    * Add picture carousel and some extra ui tweaks
    
    * Add travel card deletion mechanism with confirmation modal
    
    * Clean up and add descriptive comments

--- client/src/lib/components/CountryCard.svelte
+++ client/src/lib/components/CountryCard.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+    import type { ModalSettings } from "@skeletonlabs/skeleton";
+    import { getModalStore } from "@skeletonlabs/skeleton";
     import {
         PlusOutline,
         MinusOutline,
         ChevronDownOutline,
         ChevronUpOutline,
+        CalendarMonthOutline,
+        ClockOutline,
+        FileLinesOutline,
     } from "flowbite-svelte-icons";
     import { slide } from "svelte/transition";
     import type { CountryCardFormData } from "$lib/types/types";
@@ -14,17 +19,30 @@
     export let onDeleteCard: (id: string) => void;
     export let cardData: CountryCardFormData = { country: "" };
 
+    // Temporary for testing, of course
+    cardData.pictures = Array(5).fill(
+        "https://st3.depositphotos.com/17828278/33150/v/450/depositphotos_331503262-stock-illustration-no-image-vector-symbol-missing.jpg",
+    );
     let isExpanded = false;
+
+    const modalStore = getModalStore();
+    const openConfirmationModal = (id: string) => {
+        const deleteCardConfirmationModal: ModalSettings = {
+            type: "confirm",
+            title: "Please Confirm",
+            body: "Are you sure you want to delete this trip report?",
+            response: (res: boolean) => {
+                if (res) {
+                    onDeleteCard(id);
+                    // TODO: Invoke a wall sync?
+                }
+            },
+        };
+        modalStore.trigger(deleteCardConfirmationModal);
+    };
 
     const toggleExpand = () => {
         isExpanded = !isExpanded;
-    };
-
-    const isCardExpandable = (): boolean => {
-        // Back visible fields. TODO: Make a more scalabale function here.
-        return (
-            cardData.pictures?.length != 0 || cardData.description?.length != 0
-        );
     };
 
     const formatDate = (dateStr: string | undefined) => {
@@ -32,11 +50,20 @@
         const date = new Date(dateStr);
         return date.toLocaleDateString();
     };
+
+    const isCardExpandable = (): boolean => {
+        return (
+            cardData.pictures?.length !== 0 ||
+            cardData.description?.length !== 0
+        );
+    };
 </script>
 
 {#if isPlaceholder}
     <!-- Placeholder Card -->
-    <div class="card p-6 w-3/5 max-w-[600px] h-fit flex flex-col items-center">
+    <div
+        class="card p-6 w-3/5 max-w-[600px] h-fit flex flex-col items-center border border-dashed border-gray-500"
+    >
         <p
             class="mb-4 font-normal text-gray-700 dark:text-gray-400 leading-tight"
         >
@@ -52,24 +79,31 @@
     </div>
 {:else}
     <!-- Country Card with Data -->
-    <div class="card p-6 w-3/5 max-w-[600px] h-fit">
-        <div class="flex justify-between items-center">
+    <div class="card p-6 w-3/5 max-w-[600px] h-fit shadow-lg rounded-lg">
+        <div
+            class="flex justify-between items-center pb-2 mb-4"
+            class:border-b={isExpanded}
+        >
             <div>
-                <h2 class="text-xl font-semibold">{cardData.country}</h2>
+                <!-- Country Name -->
+                <h2 class="text-2xl font-bold">{cardData.country}</h2>
+                <!-- Date Range -->
                 {#if cardData.startDate || cardData.endDate}
-                    <p class="text-sm text-gray-500">
-                        {formatDate(cardData.startDate)}
+                    <p class="text-sm text-gray-400 flex items-center gap-2">
+                        <CalendarMonthOutline class="inline w-4 h-4" />
+                        <span>{formatDate(cardData.startDate)}</span>
                         {cardData.endDate
-                            ? `⟶ ${formatDate(cardData.endDate)}`
+                            ? ` ⟶ ${formatDate(cardData.endDate)}`
                             : ""}
                     </p>
                 {/if}
             </div>
+            <!-- Side Buttons -->
             <div class="flex items-center space-x-2">
                 <button
                     type="button"
                     class="btn-icon variant-filled-surface"
-                    on:click={() => onDeleteCard(key)}
+                    on:click={() => openConfirmationModal(key)}
                 >
                     <MinusOutline />
                 </button>
@@ -92,25 +126,43 @@
         {#if isExpanded}
             <div
                 transition:slide={{ delay: 0, duration: 250 }}
-                class="mt-4 space-y-4"
+                class="space-y-4"
             >
-                {#if cardData.description}
-                    <p class="text-gray-700 dark:text-gray-400">
-                        {cardData.description}
-                    </p>
-                {/if}
+                <!-- Picture Carousel -->
                 {#if cardData.pictures && cardData.pictures.length > 0}
-                    <div
-                        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2"
-                    >
-                        {#each cardData.pictures as picture}
-                            <img
-                                src={URL.createObjectURL(picture)}
-                                alt="Trip Picture"
-                                class="w-full h-auto object-cover"
-                                loading="lazy"
-                            />
-                        {/each}
+                    <div>
+                        <h3
+                            class="text-lg font-semibold flex items-center gap-2"
+                        >
+                            <ClockOutline class="w-5 h-5" />Memories
+                        </h3>
+                        <div
+                            class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-4"
+                        >
+                            {#each cardData.pictures as picture, i (i)}
+                                <div
+                                    class="snap-center shrink-0 card w-40 md:w-80 text-center"
+                                >
+                                    <img
+                                        class="snap-center w-full h-full rounded-container-token"
+                                        src={picture}
+                                        alt={`trip picture ${i + 1}`}
+                                        loading="lazy"
+                                    />
+                                </div>
+                            {/each}
+                        </div>
+                    </div>
+                {/if}
+                <!-- Description -->
+                {#if cardData.description}
+                    <div class="py-2 rounded-md">
+                        <h3
+                            class="text-lg font-semibold flex items-center gap-2"
+                        >
+                            <FileLinesOutline class="w-5 h-5" /> Trip Report
+                        </h3>
+                        <p class="text-gray-300">{cardData.description}</p>
                     </div>
                 {/if}
             </div>
