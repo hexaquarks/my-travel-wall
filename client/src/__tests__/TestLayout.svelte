48729c72b666c002edd4f19346c7a83feadc44f6
hexaquarks <seseriusca@gmail.com>
2024-11-17 17:13:31 -0500

    Add unit tests for country card

---
+++ client/src/__tests__/TestLayout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import {
+        initializeStores,
+        getModalStore,
+        Modal,
+    } from "@skeletonlabs/skeleton";
+    import type { SvelteComponentTyped } from "svelte";
+    import { onMount } from "svelte";
+
+    initializeStores();
+
+    export let ComponentUnderTest: typeof SvelteComponentTyped<any>;
+    export let componentProps: Record<string, any> = {};
+</script>
+
+<Modal />
+<svelte:component this={ComponentUnderTest} {...componentProps} />
