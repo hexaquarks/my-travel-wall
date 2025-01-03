ddc9cfa0c3aa7249f2673e39d70733d9beed617a
hexaquarks <seseriusca@gmail.com>
2025-01-03 16:59:28 -0500

    Add visualization mechanism for browsed images within the country picker modal

--- client/src/lib/components/ImagePreviewModal.svelte
+++ client/src/lib/components/ImagePreviewModal.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
     import { getModalStore } from "@skeletonlabs/skeleton";
+    import ImagePreviewCarousel from "./ImagePreviewCarousel.svelte";
 
     export let pictures: string[] = [];
     export let startIndex: number = 0;
-
-    enum KeyEnum {
-        Right = 39,
-        Left = 37,
-    }
-
-    const onKeyDown = (e: any) => {
-        if (e.keyCode == KeyEnum.Left) {
-            prevImage();
-        } else if (e.keyCode == KeyEnum.Right) {
-            nextImage();
-        }
-    };
 
     const modalStore = getModalStore();
     let currentIndex = startIndex;
@@ -23,62 +11,13 @@
     const closeModal = () => {
         modalStore.close();
     };
-
-    const nextImage = () => {
-        currentIndex = (currentIndex + 1) % pictures.length;
-    };
-
-    const prevImage = () => {
-        currentIndex = (currentIndex - 1 + pictures.length) % pictures.length;
-    };
 </script>
 
-<svelte:window on:keydown|preventDefault={onKeyDown} />
-
 {#if $modalStore[0]}
-    <div
-        class="fixed inset-0 bg-black/50 z-50 flex items-center justify-center p-4"
-    >
-        <div
-            class="relative rounded-lg shadow-xl max-w-3xl w-full p-4 flex flex-col items-center"
-        >
-            <div class="w-full flex justify-end mb-6 ml-16">
-                <button
-                    class="btn-icon font-normal text-2xl variant-ghost"
-                    on:click={closeModal}
-                >
-                    ×
-                </button>
-            </div>
-
-            <div class="flex items-center justify-center space-x-4">
-                {#if pictures.length > 1}
-                    <!-- Prev Button -->
-                    <button
-                        class="btn variant-ghost font-normal text-2xl"
-                        on:click={prevImage}
-                    >
-                        ‹
-                    </button>
-                {/if}
-
-                <!-- The Image -->
-                <img
-                    src={pictures[currentIndex]}
-                    alt="Preview"
-                    class="max-w-full max-h-[80vh] object-contain rounded"
-                />
-
-                {#if pictures.length > 1}
-                    <!-- Next Button -->
-                    <button
-                        class="btn variant-ghost font-normal text-2xl"
-                        on:click={nextImage}
-                    >
-                        ›
-                    </button>
-                {/if}
-            </div>
-        </div>
-    </div>
+    <ImagePreviewCarousel
+        isOpen={true}
+        {pictures}
+        startIndex={currentIndex}
+        onClose={closeModal}
+    />
 {/if}
